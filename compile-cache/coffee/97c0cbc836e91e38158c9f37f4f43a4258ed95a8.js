9227c1c22e852a9c38eec77c9e357159ada4d261
Niv Pagir <nivp@jazz.soc.huji.ac.il>
2016-09-04 18:11:36 +0300

    nothing changed, really

---
+++ compile-cache/coffee/97c0cbc836e91e38158c9f37f4f43a4258ed95a8.js
@@ -0,0 +1,10 @@
+(function() {
+  atom.commands.add({
+    'atom-workspace': 'git-plus:pull'
+  });
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiZmlsZTovLy9DOi9Vc2Vycy9uaXZwLy5hdG9tL2luaXQuY29mZmVlIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBQ0E7QUFBQSxFQUFBLElBQUksQ0FBQyxRQUFRLENBQUMsR0FBZCxDQUFrQjtBQUFBLElBQUEsZ0JBQUEsRUFDaEIsZUFEZ0I7R0FBbEIsQ0FBQSxDQUFBO0FBQUEiCn0=
+
+//# sourceURL=/C:/Users/nivp/.atom/init.coffee
---
+++ compile-cache/coffee/c2f0ad3afb29696428b9a1c4cbac95b1282524b3.js
@@ -0,0 +1,10 @@
+(function() {
+  atom.commands.add({
+    'atom-workspace': git.pull
+  });
+
+}).call(this);
+
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAiZmlsZSI6ICIiLAogICJzb3VyY2VSb290IjogIiIsCiAgInNvdXJjZXMiOiBbCiAgICAiZmlsZTovLy9DOi9Vc2Vycy9uaXZwLy5hdG9tL2luaXQuY29mZmVlIgogIF0sCiAgIm5hbWVzIjogW10sCiAgIm1hcHBpbmdzIjogIkFBQUE7QUFBQSxFQUFBLElBQUksQ0FBQyxRQUFRLENBQUMsR0FBZCxDQUFrQjtBQUFBLElBQUEsZ0JBQUEsRUFDaEIsR0FBRyxDQUFDLElBRFk7R0FBbEIsQ0FBQSxDQUFBO0FBQUEiCn0=
+
+//# sourceURL=/C:/Users/nivp/.atom/init.coffee
